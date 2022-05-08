048aaebe4354acc0d647174a02fee8351b04c730
AchrafElYaacoubi <achraf18el@gmail.com>
2022-05-08 20:45:08 +0100

    Updates

--- src/component/LoadMore/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './styles.scss'
-const LoadMore = ({loadMore}) => (
-    <div className="center">
-        <button className="load-more-btn" onClick={loadMore}>Load More</button>
-    </div>
-)
-
-export default LoadMore;
